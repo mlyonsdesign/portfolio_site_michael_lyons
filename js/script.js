6c19a8a238fdfdaf10caa26cb5889770951aee97
mlyonsdesign <75861875+mlyonsdesign@users.noreply.github.com>
2021-01-26 00:50:15 -0500

    Add else change className of x to "navtoggle"

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
       x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
     }
   }
