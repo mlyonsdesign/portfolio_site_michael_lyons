3719df25709002b873c5db9ed78f5475235ae43e
mlyonsdesign <75861875+mlyonsdesign@users.noreply.github.com>
2021-02-15 15:20:31 -0500

    Create contact.php file

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/246e25cd67.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Michael Lyons | UI/UX Designer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<form method="post" action="contact.php">
+    <textarea name="message"></textarea>
+    <input type="submit">
+</form>
+</html>
